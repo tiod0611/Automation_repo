c8efceb3abf249f8de58d65d8121dba3e9cf59af
kyeul611 <gyul611@likelion.org>
2023-02-22 21:35:57 +0900

    사용하지 않는 모듈 제거

--- 깃헙인증_자동화/DBUpdater.py
+++ 깃헙인증_자동화/DBUpdater.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
-import requests
+
 
 import pandas as pd
 
