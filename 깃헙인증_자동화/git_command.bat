23e150ac1d4f842538c84aca3bc519c6897fbdf5
kyeul611 <gyul611@likelion.org>
2023-03-13 16:27:18 +0900

    23.03.05

--- 깃헙인증_자동화/git_command.bat
+++ 깃헙인증_자동화/git_command.bat
@@ -1,6 +1,4 @@
-git add .
-git commit -m "today auto commit"
-git push origin master
+# git add .
+# git commit -m "today auto commit"
+# git push origin master
 
-# cd C:\Program Files\Google\Chrome\Application7
-# chrome.exe --remote-debugging-port=9222 --user-data-dir="C:/ChromeTEMP"
