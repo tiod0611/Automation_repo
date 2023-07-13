1e925629ceb9c93a577197f2553b6b317e00eaae
Park Kyeul <gyul611@likelion.org>
2023-07-14 00:38:52 +0900

    edit code

--- 깃헙_잔디심기_자동화/for_linux/crawler.py
+++ 깃헙_잔디심기_자동화/for_linux/crawler.py
@@ -20,6 +20,15 @@ user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTM
 options = webdriver.ChromeOptions()
 options.add_argument('user-agent=' + user_agent)
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
+options.add_argument("--headless")
+options.add_argument("window-size=1400,1500")
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument("start-maximized")
+options.add_argument("enable-automation")
+options.add_argument("--disable-infobars")
+options.add_argument("--disable-dev-shm-usage")
+
 
 class Crawler:
     def __init__(self, id, pw):
